a01279e42852ae30eeedb0e5796f934490b63416
Virgarakha <142412561+Virgarakha@users.noreply.github.com>
2024-08-29 07:45:27 +0700

    Add files via upload

---
+++ praktikum1-no1.php
@@ -0,0 +1,23 @@
+<?php 
+
+$bulan = 3;
+
+switch ($bulan) {
+    case 1 :
+        echo "Isi variabel bulan adalah January";
+        break;
+    case 2 :
+        echo "Isi variabel bulan adalah February";
+        break;
+    case 3 :
+        echo "Isi variabel bulan adalah Maret";
+        break;
+    case 4 :
+        echo "Isi variabel bulan adalah April";
+        break;
+    case 5 :
+        echo "Isi variabel bulan adalah Mei";
+        break;
+}
+
+?>
